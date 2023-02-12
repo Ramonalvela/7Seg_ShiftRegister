2505943ed47e7dbbe0dade35a4c2e9f14181d7b9
Ramonalvela <ramon_alvela@hotmail.com>
2023-02-12 16:40:11 -0400

    Version basica de uso de la Libreria. Version basica de uso estable

--- 7Seg_ShiftRegister.ino
+++ 7Seg_ShiftRegister.ino
@@ -1,17 +1,16 @@
 #include <ShiftRegisterQ.h>
 
 ShiftRegisterQ shiftR(1,2,3,true,4);
-//ShiftRegisterQ pantalla2(13,12,11,false,10);
+
 
 void setup(){
 
   Serial.begin(9600);
-  String Datos = "24.22";
+  String Datos = "HOLA";
   shiftR.setPins(7,8,9);
   shiftR.print(&Datos);
 
-//String Datos2 = "24.36";
- // pantalla2.print(&Datos2);
+
 
 
 }
