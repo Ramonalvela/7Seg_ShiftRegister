9dbd5b066a6544fc14a0f52e348bdc274900c90d
Ramonalvela <ramon_alvela@hotmail.com>
2023-02-12 01:13:58 -0400

    Funciona

--- 7Seg_ShiftRegister.ino
+++ 7Seg_ShiftRegister.ino
@@ -1,4 +1,18 @@
+#include <ShiftRegisterQ.h>
+
+ShiftRegisterQ shiftR(1,2,3,true,4);
+//ShiftRegisterQ pantalla2(13,12,11,false,10);
+
 void setup(){
+
+  Serial.begin(9600);
+  String Datos = "24.22";
+  shiftR.setPins(7,8,9);
+  shiftR.print(&Datos);
+
+//String Datos2 = "24.36";
+ // pantalla2.print(&Datos2);
+
 
 }
 void loop(){
