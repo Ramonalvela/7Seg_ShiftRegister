bd262c7b08f938ca833330930cd8f7f87f7c65c2
Ramonalvela <ramon_alvela@hotmail.com>
2023-02-15 00:35:47 -0400

    Muestra un mensaje de saludo e inicia un conteo. Una variable INT se incrementa y se pasa al Strig Datos.

--- 7Seg_ShiftRegister.ino
+++ 7Seg_ShiftRegister.ino
@@ -1,19 +1,27 @@
 #include <ShiftRegisterQ.h>
 
-ShiftRegisterQ shiftR(1,2,3,true,4);
-
+ShiftRegisterQ shiftR(7,8,9,true,4);
+int numero = -9999;
+String Datos = "";
 
 void setup(){
 
   Serial.begin(9600);
-  String Datos = "HOLA";
+ 
   shiftR.setPins(7,8,9);
-  shiftR.print(&Datos);
+  
 
+  Datos = numero;
+  
+  shiftR.print(&Datos);
 
 
 
 }
 void loop(){
-  
+  numero = numero + 1;
+  Datos = numero;
+  shiftR.print(&Datos);
+  Serial.println(numero);
+  delay(5);
 }
