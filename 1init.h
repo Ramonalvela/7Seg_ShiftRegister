f60a67f4e7e47fcc53afbad892751b8f4eb7f2ba
Ramonalvela <ramon_alvela@hotmail.com>
2023-02-22 22:16:40 -0400

    Correccion de Errores

--- 1init.h
+++ 1init.h
@@ -1,14 +1,14 @@
 int numero = 0;
 String Datos = "";
 ShiftRegisterQ shiftR(7, 8, 9, true, 4);
-double prom_a0;  //Promedio Potenciometro 0
-double a02;
-double prom_a1;
-double a16;
-double i;
-double V0ant;
-double j;
-double v0nuev;
+double prom_a0 =0;  //Promedio Potenciometro 0
+double a02=0;
+double prom_a1=0;
+double a16=0;
+double i=0;
+double V0ant=0;
+double j=0;
+double v0nuev=0;
 uint16_t veces = 1000;
 uint16_t Max = 100;
 int8_t t=1;
--- 2Funciones.h
+++ 2Funciones.h
@@ -1,8 +1,56 @@
 void saludo() {
+  Datos = "HOLA";
+  shiftR.print(&Datos);
+  delay(500);
+  Datos = "18.02";
+  shiftR.print(&Datos);
+  delay(500);
+}
+
+void leerPot() {
+  prom_a0 = 0;
+  prom_a1 = 0;
+  for (i = 1; i <= veces; i = i + 1) {
+    a02 = map(analogRead(A0), 0, 1010, 0, Max);
+    prom_a0 = (a02 + prom_a0);
+  }
+  prom_a0 = (prom_a0 / veces);
+  prom_a0 = (long)(prom_a0);
+
+  for (j = 1; j <= veces; j = j + 1) {
+    a16 = map(analogRead(A1), 0, 1010, 0, Max);
+    prom_a1 = (a16 + prom_a1);
+  }
+  prom_a1 = (prom_a1 / veces);
+  prom_a1 = (long)(prom_a1);
+}
+
+void ImprimirPantalla() {
+  int val0 = prom_a0;
+  int val1 = prom_a1;
+
+  Datos = "";
+  if (val1 < 10) {
+    Datos = "0";
+  }
+  Datos += val1;
+
+  Datos += ".";
+
+  if (val0 < 10) {
+    Datos += "0";
+  }
+  Datos += val0;
 
   shiftR.print(&Datos);
-  delay(1000);
-  Datos = "14.02";
-  shiftR.print(&Datos);
-  delay(1000);
+}
+
+void ImprimirSerial() {
+  Serial.print("Promedio A0  ");
+  Serial.print(prom_a0);
+  Serial.print("  Promedio A1  ");
+  Serial.println(prom_a1);
+  //Serial.println("--------------------------------------------------String de salida");
+  Serial.println(Datos);
+  //Serial.println("--------------------------------------------------String de salida");
 }
